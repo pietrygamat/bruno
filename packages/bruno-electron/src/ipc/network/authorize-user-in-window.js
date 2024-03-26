8aca9c9e799ddcc29ea15b418783f271cbc88a89
Mateusz Pietryga <mateusz.pietryga@assaabloy.com>
2024-03-26 16:23:13 +0100

    OAuth2: Implement OAuth 2.0 Implicit Grant
    #2056

--- packages/bruno-electron/src/ipc/network/authorize-user-in-window.js
+++ packages/bruno-electron/src/ipc/network/authorize-user-in-window.js
@@ -1,33 +1,40 @@
 const { BrowserWindow } = require('electron');
 const { preferencesUtil } = require('../../store/preferences');
 
+const openNewWindow = (session) => {
+  let allOpenWindows = BrowserWindow.getAllWindows();
+
+  // main window id is '1'
+  // get all other windows
+  let windowsExcludingMain = allOpenWindows.filter((w) => w.id != 1);
+  windowsExcludingMain.forEach((w) => {
+    w.close();
+  });
+
+  const window = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: false,
+      partition: session
+    },
+    show: false
+  });
+
+  window.on('ready-to-show', window.show.bind(window));
+
+  // We want browser window to comply with "SSL/TLS Certificate Verification" toggle in Preferences
+  window.webContents.on('certificate-error', (event, url, error, certificate, callback) => {
+    event.preventDefault();
+    callback(!preferencesUtil.shouldVerifyTls());
+  });
+
+  return window;
+};
+
 const authorizeUserInWindow = ({ authorizeUrl, callbackUrl, session }) => {
   return new Promise(async (resolve, reject) => {
     let finalUrl = null;
 
-    let allOpenWindows = BrowserWindow.getAllWindows();
-
-    // main window id is '1'
-    // get all other windows
-    let windowsExcludingMain = allOpenWindows.filter((w) => w.id != 1);
-    windowsExcludingMain.forEach((w) => {
-      w.close();
-    });
-
-    const window = new BrowserWindow({
-      webPreferences: {
-        nodeIntegration: false,
-        partition: session
-      },
-      show: false
-    });
-    window.on('ready-to-show', window.show.bind(window));
-
-    // We want browser window to comply with "SSL/TLS Certificate Verification" toggle in Preferences
-    window.webContents.on('certificate-error', (event, url, error, certificate, callback) => {
-      event.preventDefault();
-      callback(!preferencesUtil.shouldVerifyTls());
-    });
+    let window = openNewWindow(session);
 
     function onWindowRedirect(url) {
       // check if the url contains an authorization code
@@ -93,4 +100,87 @@ const authorizeUserInWindow = ({ authorizeUrl, callbackUrl, session }) => {
   });
 };
 
-module.exports = { authorizeUserInWindow };
+const authorizeUserInWindowImplicit = ({ authorizeUrl, callbackUrl, session }) => {
+  return new Promise(async (resolve, reject) => {
+    let finalUrl = null;
+
+    let window = openNewWindow(session);
+
+    const handleRedirect = (_, url) => {
+      const currentUrl = new URL(url);
+      // Skip any intermediate redirects
+      if (currentUrl.href.startsWith(new URL(callbackUrl).href)) {
+        // If the resource owner grants the access request, the authorization
+        // server issues an access token and delivers it to the client by adding
+        // the following parameters to the FRAGMENT component of the redirection
+        // URI using the "application/x-www-form-urlencoded" format:
+        //   access_token (REQUIRED),
+        //   token_type (REQUIRED),
+        //   expires_in (RECOMMENDED),
+        //   scope (OPTIONAL),
+        //   state (if sent from client)
+        // i.e. We expect the FRAGMENT component to be parsable as URLSearchParams
+        const uriFragmentWithResponse = new URLSearchParams(new URL(url).hash.slice(1));
+
+        if (uriFragmentWithResponse.has('access_token')) {
+          finalUrl = currentUrl;
+          window.close();
+        }
+
+        // If the resource owner denies the access request or if the request
+        // fails for reasons other than a missing or invalid redirection URI,
+        // the authorization server informs the client by adding the following
+        // parameters to the FRAGMENT component of the redirection URI using the
+        // "application/x-www-form-urlencoded" format:
+        //   error (REQUIRED),
+        //   errorDescription (OPTIONAL),
+        //   error_uri (OPTIONAL),
+        //   state (if sent from client)
+        if (uriFragmentWithResponse.has('error')) {
+          let errorData = {
+            message: 'Access Denied',
+            error: uriFragmentWithResponse.get('error'),
+            errorDescription: uriFragmentWithResponse.get('errorDescription'),
+            error_uri: uriFragmentWithResponse.get('error_uri')
+          };
+
+          reject(new Error(JSON.stringify(errorData)));
+          window.close();
+        }
+      }
+    };
+
+    const handleClose = () => {
+      if (finalUrl) {
+        try {
+          const uriFragmentWithToken = new URLSearchParams(new URL(finalUrl).hash.slice(1));
+          const accessToken = uriFragmentWithToken.get('access_token');
+          return resolve({ accessToken });
+        } catch (error) {
+          return reject(error);
+        }
+      } else {
+        return reject(new Error('Authorization window closed'));
+      }
+    };
+
+    // wait for the window to navigate to the callback url
+    window.webContents.on('did-navigate', handleRedirect);
+
+    window.webContents.on('will-redirect', handleRedirect);
+
+    window.on('close', handleClose);
+
+    try {
+      await window.loadURL(authorizeUrl);
+    } catch (error) {
+      reject(error);
+      window.close();
+    }
+  });
+};
+
+module.exports = {
+  authorizeUserInWindow,
+  authorizeUserInWindowImplicit
+};
